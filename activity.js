ae42f191ca811b9d4b79c6db15687d5a0216fb5a
flipxonsoft <manishjobs1999@gmail.com>
2021-04-14 21:14:59 +0530

    Document the code

--- activity.js
+++ activity.js
@@ -1,8 +1,11 @@
 let fs=require('fs');
 let path=require('path')
+
+//Taking input from user
 let input=process.argv.slice(2);
 let inputFolder=input[0];
 
+// javascript file extensions with javascript folder name
 let extensions={
     "Images":['.png','.jpg','.jpeg','.gif'],
     'Audio':['.mp3'],
@@ -10,9 +13,13 @@ let extensions={
     'Compressed':['.zip','.rar'],
     'Videos':['.mkv','.mp4']
 }
+// read the input folder
 let content=fs.readdirSync(inputFolder);
 
+//new folder path
 let extentionFolder=inputFolder;
+
+//Check folder is created or not
 function checkFolder(extension,folderPath) {
     
     for(let key in extensions){
@@ -29,19 +36,21 @@ function checkFolder(extension,folderPath) {
     return fs.existsSync(extentionFolder);
 }
 
+// create folder
 function createFolder(folder) {
     fs.mkdirSync(folder);
 }
 
+//move file form one folder to other folder
 function moveFile(oldPath,newPath) {
     fs.renameSync(oldPath,newPath);
 }
 
+// reading all file of given input folder
 for(var i=0;i<content.length;i++){
     console.log(content[i]);
     let fileExt=path.extname(content[i]); 
    console.log(fileExt);
-//    console.log(checkFolder(fileExt,inputFolder));
    if(!checkFolder(fileExt,inputFolder)){
     createFolder(extentionFolder);
     let newpath=path.join(extentionFolder,content[i]);
